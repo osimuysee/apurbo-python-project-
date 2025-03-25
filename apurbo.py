9298d9bd1da964d2c9d6aa20dde0964441a4960b
osimuysee <apurboantor3@gmail.com>
2025-03-25 23:44:51 +0600

    Add files via upload

---
+++ apurbo.py
@@ -0,0 +1,15 @@
+b = input("Enter your name:")#
+print("Your name is: ", b)
+a = int(input("enter your age "))
+print("your age is: ",a)
+#print(a>18)
+#print(a<=18)
+#print(a==18)
+#print(a!=18)
+if(a>=18):
+    print("you can drive")
+    print("yes")
+else:
+    print("you can not drive")
+    print("no")
+    print("yay!")
---
+++ main.py
@@ -0,0 +1,10 @@
+
+#strings are imutable
+#a = "!!Apurbo"
+#print(len (a))
+#print(a)
+#print(a. upper())
+#print(a. lower())
+#print(a. rstrip("!"))
+
+
